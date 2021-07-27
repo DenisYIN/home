55fa7483040e1560beb873e9f82bb1b12adad5e8
DenisYIN <yinfamin@hotmail.com>
2021-07-27 12:02:02 +0800

    Updates

---
+++ precache-manifest.0b66fade29f0e150d5016fbcb6384d4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ed1403aaa7e56865383ca4c3d9531e6",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "7e9fd6bb123da08aaba1",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "0f33d084f423ff65e053",
+    "url": "/home/static/js/2.28490de9.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.28490de9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9fd6bb123da08aaba1",
+    "url": "/home/static/js/main.1aace052.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "6ce9cf7a3cb5112230b290a87bb113ef",
+    "url": "/home/static/media/personal.6ce9cf7a.jpg"
+  },
+  {
+    "revision": "ce679e17896b61c57d6b77f1312bbb1e",
+    "url": "/home/static/media/resume.ce679e17.pdf"
+  }
+]);
